06b54963cec1c987ba725805d7d3442f39ca5902
SBJang <denlyou85@gmail.com>
2022-06-20 23:29:40 +0900

    공통 브릿지 함수/메소드 추가

--- components/layouts/PageLayout.jsx
+++ components/layouts/PageLayout.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import Toast from '@/components/common/Toast';
 import BottomMenu from '@/components/layouts/BottomMenu';
 import useStores from '@/stores/useStores';
@@ -6,19 +5,10 @@ import { observer } from 'mobx-react-lite';
 import LoginModal from '../common/Modal/LoginModal';
 import ModalContainer from '../common/Modal/ModalContainer';
 import ReLoginModal from '../common/Modal/ReLoginModal';
-import { Init } from '../../utils/Init';
 
 const PageTemplate = observer(({ children }) => {
   const { toastStore, modalStore } = useStores();
   const message = toastStore.message;
-
-  // 안드로이드 공통함수
-  const { test } = Init();
-
-  // 안드로이드 공통함수 선언
-  useEffect(() => {
-    test();
-  }, [test]);
 
   return (
     <>
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,10 +1,29 @@
 import Book from '@/components/layouts/book';
 import useStores from '@/stores/useStores';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Home() {
   const { globalStore } = useStores();
   const router = useRouter();
+
+  // 안드로이드 공통함수 선언
+  useEffect(() => {
+    if (window) {
+      /** APP->WEB */
+      window.globalFunction = function (jsonStr) {
+        console.log(jsonStr);
+        const params = JSON.parse(jsonStr);
+        // TODO .. 
+      };
+      /** WEB->APP */
+      if (window.BRIDGE && window.BRIDGE.globalMethod) {
+        const params = { prop1: 0,  prop2: 1 };
+        window.BRIDGE.globalMethod(JSON.stringify(params));
+      }
+    }
+  }, []);
+  
   return (
     <>
       <Book />
