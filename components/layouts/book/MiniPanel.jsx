83dcd4da83482150c517c6c8d8d9f0e84d91b4ac
perkid <yujun.ko@grotesq.com>
2022-10-03 23:14:14 +0900

    minipanel 애니메이션 효과 추가

--- components/layouts/book/MiniPanel.jsx
+++ components/layouts/book/MiniPanel.jsx
@@ -1,11 +1,14 @@
 import { useRouter } from 'next/router';
 import { observer } from 'mobx-react-lite';
 import useStores from '@/stores/useStores';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 const MiniPanel = observer(() => {
   const router = useRouter();
   const { container } = router.query;
   const { panelStore } = useStores();
+  const [isOpen, setIsOpen] = useState(true);
+  const ref = useRef(null);
 
   const panelName = () => {
     if (container === 'book') return '골프장리스트';
@@ -13,31 +16,63 @@ const MiniPanel = observer(() => {
     else if (container === 'alarm') return '예약오픈 알림';
   };
 
+  const handlePanel = () => {
+    if (!ref.current) return;
+    setIsOpen(!isOpen);
+    if(isOpen){
+      ref.current.style.maxHeight = '150px';
+    } else {
+      ref.current.style.maxHeight = '100px';
+    }
+  }
+
+  useEffect(()=>{
+    if(panelStore.panelHidden){
+      setIsOpen(true);
+    }
+  },[panelStore.panelHidden]);
+
   return (
     <>
       <div
         className='rollsheet-wrap'
-        onClick={() => panelStore.setPanelHidden(false)}
       >
-        <div className='rollsheet-container'>
+        <div ref={ref} className='rollsheet-container' onClick={() => handlePanel()}>
           <div className='rollsheet'>
             <div className="allim_reservation">
-              <h1 className='head-headline text-white'>
+              <h1 className='head-headline text-white' onClick={() => panelStore.setPanelHidden(false)}>
                 {panelName()}
-                <div className='arrow-icon'/>
+                <div className='arrow-icon' />
               </h1>
               <div className="allimg_icon">
                 {/* <span className="allim_num">{panelStore.checkedTeeList.size}</span> */}
               </div>
             </div>
             <div className='allim_sub'>
-              <p className='text-sub'>
+              {/* <p className='text-sub'>
                 {[...panelStore.filterCheckedTeeList]
                   ?.map(v => v.name)
                   .join(', ')}
-              </p>
+              </p> */}
+              <ul className='text-wrap'>
+                {
+                  [...panelStore.filterCheckedTeeList]?.map(v => (
+                    <li className='text-sub'>
+                      &#183; {v.name}
+                    </li>
+                  ))
+                }
+              </ul>
             </div>
             {/* <div className='handle'></div> */}
+          </div>
+          <div className='arrow-wrap'>
+            {
+              isOpen ?
+              <div className='down-arrow' />
+              :
+              <div className='up-arrow' />
+            }
           </div>
         </div>
       </div>
